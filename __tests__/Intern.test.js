8ce5605147ba2e8f69c72e8ebd3f4ccdbb951f24
DeathByDusk <elijahkanellakis@gmail.com>
2022-01-07 11:49:20 -0600

    added Intern Employee model extension tests

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,22 @@
+const Intern = require('../lib/Intern.js');
+
+test('creates an Intern object', () => {
+  const intern = new Intern('Garby', '107', 'garby@flarby.com', 'UT');
+
+  expect(intern.name).toBe('Garby');
+  expect(intern.id).toBe('107');
+  expect(intern.email).toBe('garby@flarby.com');
+  expect(intern.school).toBe('UT');
+});
+
+test('gets Intern github', () => {
+  const intern = new Intern('Garby', '107', 'garby@flarby.com', 'UT');
+
+  expect(intern.getSchool()).toBe('UT');
+});
+
+test('gets Intern Role', () => {
+  const intern = new Intern('Garby', '107', 'garby@flarby.com', 'UT');
+
+  expect(intern.getRole()).toBe('Intern');
+});

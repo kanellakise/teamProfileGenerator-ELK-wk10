58e26c30760e5ac4124371481a322328ecbe569e
DeathByDusk <elijahkanellakis@gmail.com>
2022-01-07 11:43:17 -0600

    Completed Engineer tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -8,3 +8,15 @@ test('creates an Engineer object', () => {
   expect(engineer.email).toBe('reggy@nerd.com');
   expect(engineer.github).toBe('reggycodes');
 });
+
+test('gets Engineer github', () => {
+  const engineer = new Engineer('Reginald', '106', 'reggy@nerd.com', 'reggycodes');
+
+  expect(engineer.getGithub()).toBe('reggycodes');
+});
+
+test('gets Engineer Role', () => {
+  const engineer = new Engineer('Reginald', '106', 'reggy@nerd.com', 'reggycodes');
+
+  expect(engineer.getRole()).toBe('Engineer');
+});

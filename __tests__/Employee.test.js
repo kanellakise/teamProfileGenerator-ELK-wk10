cf2eab1c7fbdf945ab7eab68666454b2d9b12d8f
DeathByDusk <elijahkanellakis@gmail.com>
2022-01-07 10:58:20 -0600

    added Employee model tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates an Employee object', () => {
+  const employee = new Employee('Phteven', '105', 'even@phteven.com')
+
+  expect(employee.name).toBe('Phteven');
+  expect(employee.id).toBe('105');
+  expect(employee.email).toBe('even@phteven.com');
+});
+
+test('gets Employee name', () => {
+  const employee = new Employee('Phteven');
+
+  expect(employee.getName()).toBe('Phteven');
+});
+
+test('gets Employee id', () => {
+  const employee = new Employee('Phteven', '105');
+
+  expect(employee.getId()).toBe('105');
+});
+
+test('gets Employee email', () => {
+  const employee = new Employee('Phteven', '105', 'even@phteven.com');
+
+  expect(employee.getEmail()).toBe('even@phteven.com');
+});
+
+test('gets Employee role', () => {
+  const employee = new Employee('Phteven', '105', 'even@phteven.com');
+
+  expect(employee.getRole()).toBe('Employee');
+})

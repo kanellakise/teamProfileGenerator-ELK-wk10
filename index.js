f25981bff64f9d22efac01776de2f97e94e3786f
DeathByDusk <elijahkanellakis@gmail.com>
2022-01-09 23:35:28 -0600

    created inquirer menu, created prompts to add manager.

--- index.js
+++ index.js
@@ -12,18 +12,20 @@ const Intern = require('./lib/Intern');
 const teamArray = [];
 
 const promptUser = () => {
+    console.log('Welcome to Team Profile Generator!')
     return inquirer
         .prompt([
             {
-                type: 'input',
+                type: 'list',
                 name: 'name',
-                message: 'Welcome to Team Profile Generator! What would you like to do?',
+                message: 'What would you like to do?',
                 choices: ['Add a Manager', 'Add an Engineer', 'Add an Intern', 'Generate Team Profile', 'Quit']
             }
         ])
         .then(answers => {
             switch (answers.menu) {
                 case 'Add a Manager':
+                    console.log("Please follow these prompts to add a Manager.");
                     addManager();
                     break;
                 case 'Add an Engineer':
@@ -41,3 +43,77 @@ const promptUser = () => {
             }
         });
 };
+
+const addManager = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "Please enter the Manager's name!",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("PLease enter a name!");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Please enter the Manager's ID.",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter an ID!");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Please enter the Manager's email.",
+                validate: emailInput => {
+                    if (emailInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter an email!");
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "Please enter the Manager's office number.",
+                validate: emailInput => {
+                    if (emailInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter an office number!");
+                        return false;
+                    }
+                }
+            }
+        ])
+        .then(managerData => {
+
+            let {
+                name,
+                id,
+                email,
+                officeNumber
+            } = managerData;
+            let manager = new Manager(name, id, email, officeNumber);
+            console.log(manager);
+
+            teamArray.push(manager);
+            console.log(teamArray);
+        })
+}
+
+promptUser();

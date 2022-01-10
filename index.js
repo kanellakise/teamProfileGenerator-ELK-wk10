c5b8a7bc1750c3d4b88e867b0e19e8afa55ded80
DeathByDusk <elijahkanellakis@gmail.com>
2022-01-09 22:33:36 -0600

    finished html generation, set up utils and index

---
+++ index.js
@@ -0,0 +1,3 @@
+const inquirer = require('inquirer');
+const genHtml = require('./src/genHtml.js');
+const { writeFile } = require('./utils/generate-site.js');
--- src/genHtml.js
+++ src/genHtml.js
@@ -1,4 +1,5 @@
-const genManager = function(manager) {
+// Generate HTML for Manager card
+const genManager = function (manager) {
   return `
   <div class="card">
     <div class="card-content">
@@ -11,6 +12,7 @@ const genManager = function(manager) {
         <div class="media-content">
           <p class="title is-4">${manager.name}</p>
           <p class="subtitle is-6">${manager.email}</p>
+          <p class="subtitle is-6">${manager.getRole()}</p>
         </div>
       </div>
 
@@ -23,7 +25,8 @@ const genManager = function(manager) {
   `;
 };
 
-const genEngineer = function(engineer) {
+// Generate HTML for Engineer card
+const genEngineer = function (engineer) {
   return `
   <div class="card">
     <div class="card-content">
@@ -36,6 +39,7 @@ const genEngineer = function(engineer) {
         <div class="media-content">
           <p class="title is-4">${engineer.name}</p>
           <p class="subtitle is-6">${engineer.email}</p>
+          <p class="subtitle is-6">${engineer.getRole()}</p>
         </div>
       </div>
 
@@ -48,7 +52,8 @@ const genEngineer = function(engineer) {
   `;
 };
 
-const genIntern = function(intern) {
+// Generate HTML for Intern card
+const genIntern = function (intern) {
   return `
   <div class="card">
     <div class="card-content">
@@ -61,6 +66,7 @@ const genIntern = function(intern) {
         <div class="media-content">
           <p class="title is-4">${intern.name}</p>
           <p class="subtitle is-6">${intern.email}</p>
+          <p class="subtitle is-6">${intern.getRole()}</p>
         </div>
       </div>
 
@@ -70,5 +76,75 @@ const genIntern = function(intern) {
       </div>
     </div>
   </div>
-  `
+  `;
 };
+
+genHtml = (data) => {
+  let pageArray = [];
+
+  for (let i = 0; i < data.length; i++) {
+    const employee = data[i];
+    const role = employee.getRole();
+
+
+    if (role === 'Manager') {
+      const managerCard = genManager(data);
+
+      pageArray.push(managerCard);
+    }
+
+    if (role === 'Engineer') {
+      const engineerCard = genEngineer(data);
+
+      pageArray.push(engineerCard);
+    }
+
+    if (role === 'Intern') {
+      const internCard = genIntern(data);
+
+      pageArray.push(internCard);
+    }
+  }
+
+  const employeeCards = pageArray.join('');
+
+  const generateTeam = generateTeamPage(employeeCards);
+
+  return generateTeam;
+
+};
+
+const generateTeamPage = function (employeeCards) {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Team Profile</title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+      <link rel="stylesheet" href="style.css">
+  </head>
+  
+  <body>
+      <header class="has-text-centered m-auto">
+          <nav class=' is-centered is-vcentered is-mobile'>
+              <span class="is-size-1 has-text-centered has-text-weight-bold">Team Profile</span>
+          </nav>
+      </header>
+      <main>
+          <div class="column">
+              <div class="row justify-content-center columns" id="team-cards">
+                  ${employeeCards}
+              </div>
+          </div>
+      </main>
+  
+  </body>
+  
+  </html>
+  `;
+};
+
+module.exports = genHtml;
